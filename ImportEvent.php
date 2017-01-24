f91e6166e9d7ebda218597b0628a34e45b6961f8
jhansonpk <jhansonpk@hotmail.com>
2017-01-24 14:21:00 -0200

    init class

---
+++ ImportEvent.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jhans
+ * Date: 24/01/2017
+ * Time: 13:59
+ */
+
+namespace arroios\plugins;
+
+Class ImportEvent
+{
+    public $facebook_id;
+    public $facebook_secret;
+
+    function __construct($conf)
+    {
+
+    }
+}

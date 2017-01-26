4670d7f1994378ecb7beec5e70b9f93f736cf666
jhansonpk <jhansonpk@hotmail.com>
2017-01-26 18:28:37 -0200

    update database

--- Database.php
+++ Database.php
@@ -15,7 +15,7 @@ class Database
     public static function getConnection()
     {
         if (self::$conn === NULL) {
-            $dsn = 'sqlite:'.__DIR__.'/db/plugin.sqlite';
+            $dsn = 'sqlite:'.__DIR__.'/db/plugin_v1.sqlite';
 
             self::$conn = new PDO($dsn);
 
@@ -24,8 +24,8 @@ class Database
             }
 
             self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            self::$conn->exec("CREATE TABLE IF NOT EXISTS Page (id INTEGER PRIMARY KEY, token TEXT, userId INT)");
-            self::$conn->exec("CREATE TABLE IF NOT EXISTS Event (id INTEGER PRIMARY KEY, updateTime TEXT, pageId INT)");
+            self::$conn->exec("CREATE TABLE IF NOT EXISTS Page (facebookPageId INTEGER PRIMARY KEY, facebookToken TEXT, facebookPageName TEXT)");
+            self::$conn->exec("CREATE TABLE IF NOT EXISTS Event (facebookEventId INTEGER PRIMARY KEY, facebookEventUpdateTime TEXT, facebookPageId INT, longitude TEXT, latitude TEXT, zip TEXT, street TEXT, city TEXT, state TEXT, place TEXT, cover TEXT, description TEXT, name TEXT, endTime TEXT,  startTime TEXT)");
         }
         return self::$conn;
     }
